bddc5a3bff7e1e4b88d4de6f4e23c27b5687d076
manu-180 <manumanu97@hotmail.com>
2026-01-31 18:44:16 -0300

    vercel

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -65,7 +65,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "9cc4498ad5e38829b98ce43471e7dfba
 "favicons/favicon_riverpod.png": "6725cb0a7c912366c40ecdc22bd2fc62",
 "favicons/favicon_supabase.png": "86621263c2beee234fe1d5135b8909a0",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
-"flutter_bootstrap.js": "e5bf23db6a2b40d22c46c3be53c2231a",
+"flutter_bootstrap.js": "d41704abbb17a5d94aecb4253c4a2c8a",
 "icons/Icon-192.png": "60ed491803bfd2a163b92db45a9e2f64",
 "icons/Icon-512.png": "f0a96202bd8495d5ebf56de07231ce4a",
 "icons/Icon-maskable-192.png": "60ed491803bfd2a163b92db45a9e2f64",
