8242dbbbc834b7604464320c8da8c20ba4300114
manu-180 <manumanu97@hotmail.com>
2026-01-30 23:21:08 -0300

    vercel

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"78fc3012e45889657f72359b005af7beac47ba
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2737886495"
+    serviceWorkerVersion: "151824295"
   }
 });
